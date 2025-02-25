cff01c27d229e8b19bef6ec16488e068422b0a9c
Cezary Sosialuk <c.sosialuk1@gmail.com>
2025-02-25 10:23:22 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+I=1
+V=5
+X=10
+L=50
+C=100
+D=500
+M=1000
+
+0 < num < 4000
+*/
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string rom;
+
+        }
+        static int RomToInt(string rn)
+        {
+            int n = 0;
+
+            return n;
+        }
+    }
+}
